abb89c9f8e701b50889265afc19ca92aae1bda40
Mr-D3ath <Mr-D3ath@users.noreply.github.com>
2025-11-18 17:24:24 +0530

    The First Assignment

---
+++ Assignment_1.py
@@ -0,0 +1,18 @@
+# First Question - Perform Basic Mathematical Operations
+
+first = int(input("Enter your first number: "))
+second = int(input("Enter your second number: "))
+
+print("Addition: ", first + second)
+print("Subtraction: ", first - second)
+print("Multiplication: ", first * second)
+print("Division: ", first / second)
+
+# Second Question - Create a Personalized Greeting
+
+First_name = input("ENter Your First name: ")
+Last_name = input("Enter Your Last name: ")
+full_name = (First_name + " " + Last_name)
+
+print(f"Hello, {full_name}! Welcome to the Python Program.")
+
